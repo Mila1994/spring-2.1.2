b1438feb7b8308848cc12c190b11d51ce1fe55f6
madammelon20 <madammelon20@gmail.com>
2025-01-08 23:57:49 +0200

    Задание Spring 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("HelloWorld beans are the same: " + (bean1 == bean2));
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat beans are the same: " + (cat1 == cat2));
+
+
+
     }
+
+
 }
